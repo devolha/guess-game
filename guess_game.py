aef17a104d1bba032369d2bed30d85a3baa17829
devolha <sokolowskaoa@gmail.com>
2025-03-16 15:22:52 +0100

    changed var name for clearity

--- guess_game.py
+++ guess_game.py
@@ -1,10 +1,11 @@
 def get_user_guess():
     import random
-    number = random.randint(1,10)
-    print(f"Comment: Guess number is {number}")
+    guess_number = random.randint(1,10)
+    print(f"Comment: Guess number is {guess_number}")
     print()
     user_input = int(input("Let's play Number Guess Game! Enter a number from 1 to 10: "))
-    return number, user_input
+    return guess_number, user_input
+
 
     
 get_user_guess()
