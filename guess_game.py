2b9eebe25e117abe87d480819c34927befc27f22
devolha <sokolowskaoa@gmail.com>
2025-03-16 18:43:11 +0100

    logic for user guesses with multiple conditionals

--- guess_game.py
+++ guess_game.py
@@ -1,11 +1,24 @@
+from random import randint
+
 def get_user_guess():
-    import random
-    guess_number = random.randint(1,10)
-    print(f"Comment: Guess number is {guess_number}")
-    print()
-    user_input = int(input("Let's play Number Guess Game! Enter a number from 1 to 10: "))
-    return guess_number, user_input
+    guess_number = randint(1,10)
+    return guess_number
 
+guess_number = get_user_guess()
+print(f"Let's play Number Guess Game!\n Comment: Guess number is {guess_number}\n")
 
-    
-get_user_guess()
+while True:
+    try:
+        user_input = int(input("Enter a number from 1 to 10: "))
+        if user_input not in range(1,10):
+            print('Please enter a number between 1 and 10: ')
+        elif user_input == guess_number:
+            print(f'Congratulations! It was {guess_number}!')
+            break
+        elif user_input < guess_number:
+            print('Your number is lower then need. Try one more time: ')
+        elif user_input > guess_number:
+            print('Your number is higher then need. Try one more time: ')
+    except (ValueError, SyntaxError, NameError):
+        print("It's not a number. Try one more time!")
+  
