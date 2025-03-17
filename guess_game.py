bf69c9a7dd8302a81f5ae59f61349cd56890cb7a
Vołha Sakaloŭskaja <sokolowskaoa@gmail.com>
2025-03-17 07:39:23 +0100

    Update guess_game.py
    Syntax errors fixed

--- guess_game.py
+++ guess_game.py
@@ -9,16 +9,16 @@ print(f"Let's play a Number Guess Game!\nComment: Guess number is {guess_number}
 
 while True:
     try:
-        user_input = int(input("Enter a number from 1 to 10: "))
+        user_input = int(input("Enter a number from 1 to 10:"))
         if user_input not in range(1,11):
-            print('Please enter a number between 1 and 10: ')
+            print('Please enter a number between 1 and 10.')
         elif user_input == guess_number:
             print(f'Congratulations! It was {guess_number}!')
             break
         elif user_input < guess_number:
-            print('Your number is lower then need. Try one more time: ')
+            print('Your number is lower than needed. Try one more time.')
         elif user_input > guess_number:
-            print('Your number is higher then need. Try one more time: ')
+            print('Your number is higher than needed. Try one more time.')
     except (ValueError, SyntaxError, NameError):
         print("It's not a number. Try one more time!")
   
